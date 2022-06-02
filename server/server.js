f0e06d1306334d14ae5064e45ab499d0438e164e
Damon medek <68669987+DamonMedek@users.noreply.github.com>
2022-06-02 13:28:20 -0700

    basic layout

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
+//APP
 const express = require("express");
 const app = express();
+
+//MONGOOSE
+const mongoose = require("mongoose");
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
@@ -8,3 +12,12 @@ app.get("/api", (req, res) => {
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
+
+/*//DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected").catch((err) => console.log("DB Connection Error", err)));
+*/
